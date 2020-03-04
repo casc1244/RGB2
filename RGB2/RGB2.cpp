60cbfb2700b5a9a0c479d957d58165f19ee65b09
casc1244 <1356602646@qq.com>
2020-03-04 22:09:51 +0800

    RGB2.cpp

--- RGB2/RGB2.cpp
+++ RGB2/RGB2.cpp
@@ -1,20 +1,30 @@
-﻿// RGB2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+using namespace cv;
+using namespace std;
+int main() {
+	VideoCapture cap;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	cap.open(0);
+	if (!cap.isOpened()) {
+		std::cout << "不能打开视频文件" << std::endl;
+		return -1;
+	}
+
+	double  fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1) {
+		cv::Mat frame;
+		bool rSucess = cap.read(frame);
+		if (!rSucess) {
+			std::cout << "不能从视频文件中读取帧" << std::endl;
+			break;
+		}
+		else {
+			cv::imshow("frame", frame);
+		}
+		waitKey(30);
+	}
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
